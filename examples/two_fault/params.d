44d5603934e88f81e83e9cefcf3f7def37de8999
Kasey W. Schultz <kwschultz@ucdavis.edu>
2014-02-20 14:43:56 -0800

    Documentation is semi-finalized. Once VC is running, the manual will be completed by adding simulation results to the Tutorials chapter.

---
+++ examples/two_fault/params.d
@@ -0,0 +1,11 @@
+sim.version                       = 2.0
+sim.time.end_year                 = 10000
+sim.greens.method                 = standard
+sim.greens.use_normal             = false
+sim.friction.dynamic              = DYN
+sim.file.input                    = OUTFILE.txt
+sim.file.input_type               = text
+sim.eqsim.file.friction           = ../multiple_friction.dat
+sim.file.output_event             = events_RES.txt
+sim.file.output_sweep             = sweeps_RES.txt
+sim.file.output_event_type        = text
---
+++ examples/two_fault/setup_params.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ELEM_SIZE=$1
+TAPER_METHOD=$2
+DYNAMIC=$3
+
+echo ""
+echo "dynamic triggering factor:   $DYNAMIC"
+echo "taper method:                $TAPER_METHOD"
+echo "element size [m]:            $ELEM_SIZE"
+echo "****parameter file written:  $TAPER_METHOD/params_$ELEM_SIZE.d"
+echo ""
+
+BASE_NAME=two_fault_$ELEM_SIZE
+
+sed -e s/DYN/$DYNAMIC/ \
+    -e s/RES/$ELEM_SIZE/g \
+    -e s/OUTFILE/$BASE_NAME/ <params.d >$TAPER_METHOD/params_$ELEM_SIZE.d
+
+exit $?
