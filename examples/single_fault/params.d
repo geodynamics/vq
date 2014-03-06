2f7b1d46d502da7e77bc227bb5dce24fb3fb026c
Eric Heien <github@heien.org>
2014-03-05 16:55:55 -0800

    Fix CMake tests

--- examples/single_fault/params.d
+++ examples/single_fault/params.d
@@ -5,7 +5,6 @@ sim.greens.use_normal             = false
 sim.friction.dynamic              = DYN
 sim.file.input                    = OUTFILE.txt
 sim.file.input_type               = text
-sim.eqsim.file.friction           = ../single_friction.dat
 sim.file.output_event             = events_RES.txt
 sim.file.output_sweep             = sweeps_RES.txt
 sim.file.output_event_type        = text
