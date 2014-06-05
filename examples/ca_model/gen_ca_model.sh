a63ca3da074f8f7cd756d6195e6f7a4b17455988
kwschultz <kwschultz@ucdavis.edu>
2014-06-05 15:27:35 -0700

    Addendum to manual

--- examples/ca_model/gen_ca_model.sh
+++ examples/ca_model/gen_ca_model.sh
@@ -10,7 +10,16 @@ ELEM_SIZE=$1
 
 # Create the command line argument based on the list of trace files in the directory
 #FILE_LIST=`ls ../fault_traces/ca_traces/trace_Greenvalley.txt ../fault_traces/ca_traces/trace_Hayward.txt`
+
+# The list below grabs all California fault sections into
+#    one large fault model.
 FILE_LIST=`ls ../fault_traces/ca_traces/trace_*.txt`
+
+
+# Additional geometry file formats
+#    --export_file_type=hdf5 \
+#    --export_file=all_cal.h5 \
+
 
 EDITOR_ARGS=
 for FILE in $FILE_LIST
@@ -21,11 +30,8 @@ done
 ../../build/src/mesher $EDITOR_ARGS \
     --export_file=all_cal.txt \
     --export_file_type=text \
-    --export_file=all_cal.h5 \
-    --export_file_type=hdf5 \
     --export_file=all_cal.kml \
     --export_file_type=kml \
     --export_eqsim_geometry=all_cal_geom_${ELEM_SIZE}.dat \
-    --export_eqsim_friction=all_cal_fric_${ELEM_SIZE}.dat \
     --merge_duplicate_verts
 
