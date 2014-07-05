52af790ab1d3d3bac90d1356f5f10241727c133e
Eric Heien <github@heien.org>
2014-07-05 16:14:00 -0700

    Fix param generation script

--- examples/two_fault/setup_params.sh
+++ examples/two_fault/setup_params.sh
@@ -6,14 +6,14 @@ DYNAMIC=$2
 echo ""
 echo "dynamic triggering factor:   $DYNAMIC"
 echo "element size [m]:            $ELEM_SIZE"
-echo "****parameter file written:  params_$ELEM_SIZE.d"
+echo "parameter file written:  params_two_$ELEM_SIZE.d"
 echo ""
 
 BASE_NAME=two_fault_$ELEM_SIZE
 
 sed -e s/DYN/$DYNAMIC/ \
     -e s/RES/$ELEM_SIZE/g \
-    -e s/OUTFILE/$BASE_NAME/ < ../../../examples/diff_fault/params.d > params_two_$ELEM_SIZE.d
+    -e s/OUTFILE/$BASE_NAME/ < ../../../../examples/two_fault/params.d > params_two_$ELEM_SIZE.d
 
 exit $?
 
