68f505d1c8f288a0762fd8a7cc0c94d84be567ed
Eric Heien <github@heien.org>
2014-07-05 15:13:20 -0700

    Fix single fault test params

--- examples/single_fault/setup_params.sh
+++ examples/single_fault/setup_params.sh
@@ -4,16 +4,16 @@ ELEM_SIZE=$1
 DYNAMIC=$2
 
 echo ""
-echo "dynamic triggering factor:   $DYNAMIC"
-echo "element size [m]:            $ELEM_SIZE"
-echo "****parameter file written:  params_$ELEM_SIZE.d"
+echo "dynamic triggering factor:   ${DYNAMIC}"
+echo "element size [m]:            ${ELEM_SIZE}"
+echo "parameter file written:      params_${ELEM_SIZE}.d"
 echo ""
 
 BASE_NAME=single_fault_$ELEM_SIZE
 
-sed -e s/DYN/$DYNAMIC/ \
-    -e s/RES/$ELEM_SIZE/g \
-    -e s/OUTFILE/$BASE_NAME/ <../../../examples/single_fault/params.d >params_$ELEM_SIZE.d
+sed -e s/DYNAMIC/${DYNAMIC}/g \
+    -e s/ELEM_SIZE/${ELEM_SIZE}/g \
+    -e s/INPUTFILE/${BASE_NAME}/g < ../../../../examples/single_fault/params.d > params_${ELEM_SIZE}.d
 
 exit $?
 
