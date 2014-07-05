595c04f5a112de8a89d41d286699b82ae6ecb178
Eric Heien <github@heien.org>
2014-07-05 16:14:11 -0700

    Refix CA model generator

--- examples/ca_model/gen_ca_model.sh
+++ examples/ca_model/gen_ca_model.sh
@@ -11,17 +11,9 @@ ELEM_SIZE=$1
 # Create the command line argument based on the list of trace files in the directory
 #FILE_LIST=`ls ../fault_traces/ca_traces/trace_SAF-Parkfield.txt ../fault_traces/ca_traces/trace_Hayward.txt`
 
-FILE_LIST='../../fault_traces/two_fault_1.txt ../../fault_traces/two_fault_2.txt'
-
 # The list below grabs all California fault sections into
 #    one large fault model.
-#FILE_LIST=`ls ../fault_traces/ca_traces/trace_*.txt`
-
-
-# Additional geometry file formats
-#    --export_file_type=hdf5 \
-#    --export_file=all_cal.h5 \
-#    --export_eqsim_geometry=all_cal_${ELEM_SIZE}.dat \
+FILE_LIST=`ls ../fault_traces/ca_traces/trace_*.txt`
 
 EDITOR_ARGS=
 for FILE in $FILE_LIST
@@ -29,10 +21,13 @@ do
     EDITOR_ARGS="$EDITOR_ARGS--import_file=$FILE --import_file_type=trace --import_trace_element_size=$ELEM_SIZE --taper_fault_method=none "
 done
 
-../../../build/src/mesher $EDITOR_ARGS \
-    --export_file=two_fault_${ELEM_SIZE}.txt \
+../../../../build/src/mesher $EDITOR_ARGS \
+    --export_file=all_cal_fault_${ELEM_SIZE}.txt \
     --export_file_type=text \
-    --export_file=two_fault_{ELEM_SIZE}.kml \
+    --export_file=all_cal_fault_${ELEM_SIZE}.kml \
     --export_file_type=kml \
+    --export_file=all_cal_fault_${ELEM_SIZE}.h5 \
+    --export_file_type=hdf5 \
+    --export_eqsim_geometry=all_cal_${ELEM_SIZE}.dat \
     --merge_duplicate_verts
 
