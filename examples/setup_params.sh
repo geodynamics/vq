07ebb05f53c5e8acfc0a34b1a35f6d94de9fb5ea
Eric Heien <github@heien.org>
2014-07-06 14:47:27 -0700

    Combine single/double fault test framework

--- examples/setup_params.sh
+++ examples/setup_params.sh
@@ -2,6 +2,8 @@
 
 ELEM_SIZE=$1
 DYNAMIC=$2
+PREFIX=$3
+PARAM_TEMPLATE=$4
 
 echo ""
 echo "dynamic triggering factor:   ${DYNAMIC}"
@@ -9,11 +11,11 @@ echo "element size [m]:            ${ELEM_SIZE}"
 echo "parameter file written:      params_${ELEM_SIZE}.d"
 echo ""
 
-BASE_NAME=single_fault_$ELEM_SIZE
+BASE_NAME=${PREFIX}_${ELEM_SIZE}
 
 sed -e s/DYNAMIC/${DYNAMIC}/g \
     -e s/ELEM_SIZE/${ELEM_SIZE}/g \
-    -e s/INPUTFILE/${BASE_NAME}/g < ../../../../examples/single_fault/params.d > params_${ELEM_SIZE}.d
+    -e s/INPUTFILE/${BASE_NAME}/g < ${PARAM_TEMPLATE} > params_${ELEM_SIZE}.d
 
 exit $?
 
