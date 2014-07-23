f4d4b70a6de71db03ee98c41fb523620ebb6194e
Eric Heien <github@heien.org>
2014-07-23 10:45:06 +0900

    Update to next release version

--- quakelib/python/__init__.py
+++ quakelib/python/__init__.py
@@ -1,11 +1,11 @@
-"""Quakelib Earthquake Simulation Library
+"""QuakeLib Earthquake Simulation Library
 """
 
 __all__ = ['quakelib']
 
-__version__ = '1.0'
+__version__ = '1.1.0'
 
-version_info = (1, 0, 0, 'final', 0)
+version_info = (1, 1, 0, 'final', 0)
 
 from quakelib import *
 
