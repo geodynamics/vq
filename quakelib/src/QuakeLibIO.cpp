3b3d5a641abff02d465378f25d72273edf670508
Eric Heien <emheien@ucdavis.edu>
2014-10-15 14:17:59 -0700

    astyle cleanup

--- quakelib/src/QuakeLibIO.cpp
+++ quakelib/src/QuakeLibIO.cpp
@@ -1601,7 +1601,7 @@ int quakelib::ModelWorld::read_files_eqsim(const std::string &geom_file_name, co
 
     // Take the conversion base as the middle of the section map
     base = quakelib::LatLonDepth(geometry_data.lat_lo(), geometry_data.lon_lo());
-    
+
     // Initiate converter
     Conversion          conv(base);
 
@@ -1628,31 +1628,31 @@ int quakelib::ModelWorld::read_files_eqsim(const std::string &geom_file_name, co
             new_element.set_lame_mu(friction_data.get_lame_mu());
             //new_element.set_static_strength(friction_data.get_static_strength(it->first));
             //new_element.set_dynamic_strength(friction_data.get_dynamic_strength(it->first));
-            
+
             // Insert partially finished element into the eqsim_world
             eqsim_world.insert(new_element);
-            
+
             // Compute area of the current element, add it to the total for this section
             fault_areas[sit->second.sid()] += eqsim_world.create_sim_element(new_element.id()).area();
-        
-        
+
+
         }
-        
+
         // Go through the created elements and assign maximum slip based on fault section area
-        for (eit=eqsim_world.begin_element();eit!=eqsim_world.end_element();++eit) {
-        
+        for (eit=eqsim_world.begin_element(); eit!=eqsim_world.end_element(); ++eit) {
+
             // From Table 2A in Wells Coppersmith 1994
             double moment_magnitude = 4.07+0.98*log10(conv.sqm2sqkm(fault_areas[eit->section_id()]));
-            
-            // Need to document where this scaling law comes from 
+
+            // Need to document where this scaling law comes from
             double max_slip = pow(10, (3.0/2.0)*(moment_magnitude+10.7))/(1e7*(eit->lame_mu())*fault_areas[eit->section_id()]);
-            
+
             // Set the max slip for the current element
             eit->set_max_slip(max_slip);
-            
-            }
-        
-        
+
+        }
+
+
     }
 
     insert(eqsim_world);
