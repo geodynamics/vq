06cab4f7137d90fda20791da78fcb4aa16b27f15
Eric Heien <emheien@ucdavis.edu>
2014-12-04 13:50:30 -0800

    Improve param generation script

--- examples/setup_params.sh
+++ examples/setup_params.sh
@@ -4,6 +4,7 @@ ELEM_SIZE=$1
 DYNAMIC=$2
 PREFIX=$3
 PARAM_TEMPLATE=$4
+PARAM_OUTPUT=$5
 
 echo ""
 echo "dynamic triggering factor:   ${DYNAMIC}"
@@ -15,7 +16,7 @@ BASE_NAME=${PREFIX}_${ELEM_SIZE}
 
 sed -e s/DYNAMIC/${DYNAMIC}/g \
     -e s/ELEM_SIZE/${ELEM_SIZE}/g \
-    -e s/INPUTFILE/${BASE_NAME}/g < ${PARAM_TEMPLATE} > params_${ELEM_SIZE}.d
+    -e s/INPUTFILE/${BASE_NAME}/g < ${PARAM_TEMPLATE} > ${PARAM_OUTPUT}
 
 exit $?
 
