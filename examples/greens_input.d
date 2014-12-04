b2163befde8b3742868ecc66e752fe4cd0f02524
Eric Heien <emheien@ucdavis.edu>
2014-12-04 13:35:26 -0800

    Add Greens file input/output tests

---
+++ examples/greens_input.d
@@ -0,0 +1,10 @@
+sim.version                       = 2.0
+sim.time.end_year                 = 10000
+sim.greens.method                 = file
+sim.greens.input                  = greens_ELEM_SIZE.h5
+sim.friction.dynamic              = DYNAMIC
+sim.file.input                    = INPUTFILE.txt
+sim.file.input_type               = text
+sim.file.output_event             = events_ELEM_SIZE.txt
+sim.file.output_sweep             = sweeps_ELEM_SIZE.txt
+sim.file.output_event_type        = text
