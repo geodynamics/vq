8e77a920d0a548ec9a12b9f8a6f4891eded1d201
Eric Heien <emheien@ucdavis.edu>
2015-01-02 11:29:27 -0800

    Add KML output

--- quakelib/src/QuakeLibIO.cpp
+++ quakelib/src/QuakeLibIO.cpp
@@ -1844,8 +1844,10 @@ int quakelib::ModelWorld::write_file_kml(const std::string &file_name) {
                 // Output the KML format polygon for this section
                 out_file << "\t\t<Placemark>\n";
                 out_file << "\t\t<description>\n";
+                out_file << "Fault name: " << fit->second.name() << "\n";
                 out_file << "Slip rate: " << c.m_per_sec2cm_per_yr(eit->second.slip_rate()) << " cm/year\n";
                 out_file << "Rake: " << c.rad2deg(eit->second.rake()) << " degrees\n";
+                out_file << "Aseismic: " << eit->second.aseismic() << "\n";
                 out_file << "\t\t</description>\n";
                 out_file << "\t\t\t<styleUrl>#baseStyle</styleUrl>\n";
                 out_file << "\t\t\t<Polygon>\n";
