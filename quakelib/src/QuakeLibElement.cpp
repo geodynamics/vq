1a5cb1b60e5fc9d8e716ec3feb5e15108abfab5b
kwschultz <kwschultz@ucdavis.edu>
2015-04-10 14:51:00 -0700

    Okubo greens function fix

--- quakelib/src/QuakeLibElement.cpp
+++ quakelib/src/QuakeLibElement.cpp
@@ -210,7 +210,7 @@ quakelib::FloatList quakelib::SlipMap::gravity_changes(const VectorList &points,
 
     for (SlippedElementList::size_type ele_id = 0; ele_id != involved_elements.size(); ele_id++) {
         slip = involved_elements[ele_id].slip();
-        c = involved_elements[ele_id].max_depth();
+        c = fabs(involved_elements[ele_id].max_depth());
         rake_cos = cos(involved_elements[ele_id].rake());
         rake_sin = sin(involved_elements[ele_id].rake());
 
@@ -248,6 +248,24 @@ quakelib::FloatList quakelib::SlipMap::gravity_changes(const VectorList &points,
 
         xp3 = involved_elements[ele_id].implicit_vert()[0];
         yp3 = involved_elements[ele_id].implicit_vert()[1];
+        
+        
+        std::cout << "v1: <" << involved_elements[ele_id].vert(0)[0] << ", " << involved_elements[ele_id].vert(0)[1] << ", " << involved_elements[ele_id].vert(0)[2] << ">" << std::endl;
+        std::cout << "v2: <" << involved_elements[ele_id].vert(1)[0] << ", " << involved_elements[ele_id].vert(1)[1] << ", " << involved_elements[ele_id].vert(1)[2] << ">" << std::endl;
+        std::cout << "v3: <" << involved_elements[ele_id].vert(2)[0] << ", " << involved_elements[ele_id].vert(2)[1] << ", " << involved_elements[ele_id].vert(2)[2] << ">" << std::endl;
+        std::cout << "v4: <" << involved_elements[ele_id].implicit_vert()[0] << ", " << involved_elements[ele_id].implicit_vert()[1] << ", " << involved_elements[ele_id].implicit_vert()[2] << ">" << std::endl;
+        std::cout << "L: " << L << std::endl;
+        std::cout << "W: " << W << std::endl;
+        std::cout << "c: " << c << std::endl;
+        std::cout << "US: " << US << std::endl;
+        std::cout << "UD: " << UD << std::endl;
+        std::cout << "UT: " << UT << std::endl;
+        std::cout << "dip: " << dip << std::endl;
+        std::cout << "rake: " << involved_elements[ele_id].rake() << std::endl;
+        std::cout << "strike: " << strike << std::endl;
+        std::cout << "xp0: " << xp0 << std::endl;
+        std::cout << "yp0: " << yp0 << std::endl;
+
 
         for (VectorList::size_type point_id = 0; point_id != points.size(); point_id++) {
             x = points[point_id][0];
@@ -543,5 +561,3 @@ quakelib::FloatList quakelib::SlipMap::potential_changes(const VectorList &point
 
     return potential_changes;
 }
-
-
