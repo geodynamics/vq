a0682c9b3a9199e0b38372717bd681def3dcbc37
kwschultz <kwschultz@ucdavis.edu>
2015-04-13 18:26:05 -0700

    Commented out debugging code

--- quakelib/src/QuakeLibElement.cpp
+++ quakelib/src/QuakeLibElement.cpp
@@ -249,7 +249,7 @@ quakelib::FloatList quakelib::SlipMap::gravity_changes(const VectorList &points,
         xp3 = involved_elements[ele_id].implicit_vert()[0];
         yp3 = involved_elements[ele_id].implicit_vert()[1];
         
-        
+        /*
         std::cout << "v1: <" << involved_elements[ele_id].vert(0)[0] << ", " << involved_elements[ele_id].vert(0)[1] << ", " << involved_elements[ele_id].vert(0)[2] << ">" << std::endl;
         std::cout << "v2: <" << involved_elements[ele_id].vert(1)[0] << ", " << involved_elements[ele_id].vert(1)[1] << ", " << involved_elements[ele_id].vert(1)[2] << ">" << std::endl;
         std::cout << "v3: <" << involved_elements[ele_id].vert(2)[0] << ", " << involved_elements[ele_id].vert(2)[1] << ", " << involved_elements[ele_id].vert(2)[2] << ">" << std::endl;
@@ -260,12 +260,12 @@ quakelib::FloatList quakelib::SlipMap::gravity_changes(const VectorList &points,
         std::cout << "US: " << US << std::endl;
         std::cout << "UD: " << UD << std::endl;
         std::cout << "UT: " << UT << std::endl;
-        std::cout << "dip: " << dip << std::endl;
-        std::cout << "rake: " << involved_elements[ele_id].rake() << std::endl;
-        std::cout << "strike: " << strike << std::endl;
+        std::cout << "dip: " << dip*180.0/M_PI << std::endl;
+        std::cout << "rake: " << involved_elements[ele_id].rake()*180.0/M_PI << std::endl;
+        std::cout << "strike: " << strike*180.0/M_PI << std::endl;
         std::cout << "xp0: " << xp0 << std::endl;
         std::cout << "yp0: " << yp0 << std::endl;
-
+        */
 
         for (VectorList::size_type point_id = 0; point_id != points.size(); point_id++) {
             x = points[point_id][0];
