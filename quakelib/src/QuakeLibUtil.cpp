76c22f40ba97c082c0803e31304747b83f3b9233
johnmaxwilson <johnmaxwilson@gmail.com>
2015-11-19 16:22:53 -0800

    Ensure mesher produces square elements; Added comments for functions for halfspace trace input.

--- quakelib/src/QuakeLibUtil.cpp
+++ quakelib/src/QuakeLibUtil.cpp
@@ -187,6 +187,7 @@ quakelib::Vec<3> quakelib::Conversion::convert2xyz(const LatLonDepth &in_pt) con
 #endif
 
 quakelib::Vec<3> quakelib::Conversion::yxz2xyz(const LatLonDepth &in_pt) const {
+	//Use for importing trace in (y, x) halfspace coords.  Must turn off LatLonDepth error throwning for angles out of bounds in QuakeLibUtil.h.
     double  new_vals[3];
 
     new_vals[0] = in_pt.lon();
