a7e6635b34cc34376e89998d9acf618aefe34e08
John Wilson <johnmaxwilson@gmail.com>
2017-06-20 17:43:07 -0700

    docker: mac users no longer need to pass uid and gid to the container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-groupmod --gid=$HOST_GID virtualquake
-usermod --uid=$HOST_UID virtualquake
+if [ -n "${HOST_UID}" ]
+  then
+    groupmod --gid=$HOST_GID virtualquake
+    usermod --uid=$HOST_UID virtualquake
+fi
 chown -R virtualquake:virtualquake /home/virtualquake/
 cd /home/virtualquake/external_volume
 su virtualquake
